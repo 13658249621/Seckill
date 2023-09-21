aad685cba44dd1d1fd6ffcd6e9c7d03ab3b365fe
jiangyinshan <1326714156@qq.com>
2023-09-21 20:01:40 +0800

    [test]去掉排序测试并发性能

--- src/main/java/cn/hfbin/seckill/service/ipml/UserServiceImpl.java
+++ src/main/java/cn/hfbin/seckill/service/ipml/UserServiceImpl.java
@@ -60,19 +60,6 @@ public class UserServiceImpl implements UserService {
 
     //性能测试接口，查询用户数据，写入缓存，缓存过期时间5秒
     public Result<List<User>> performanceTest(String keyword) throws Exception {
-        int[] arr = {64, 34, 25, 12, 22, 11, 90, 87, 76, 56, 45, 32, 21, 98, 89, 73, 61, 49, 37, 28, 15, 8, 3, 0, -5};
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // 交换 arr[j] 和 arr[j + 1]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-        mqSender.sendUserMessage(EncryptionUtils.encrypt(keyword));
     List<User> userList = redisService.get(UserKey.getByKeyword, keyword, List.class);
         if (userList != null) {
             System.out.println("使用缓存");
